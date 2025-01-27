99fdd2adfa76cf5cc4f7dc9bde46515d690d0367
Vsevolod Nedora <fizik.vlk@gmail.com>
2025-01-27 10:41:04 +0100

    fixes

--- publish_data.py
+++ publish_data.py
@@ -195,8 +195,6 @@ def publish_to_api(
 
     save_to_json(df_results, metadata, fpath_total_json, verbose)
 
-
-
 def publish_generation(
         target='wind_offshore',
         avail_regions=('DE_50HZ', 'DE_TENNET'),
@@ -454,290 +452,54 @@ Die SMARD __Tagesprognose__ weist eine durchschnittliche Genauigkeit von __{ave_
     with open(summary_fpath, "w") as file:
         file.write(updated_markdown_content)
 
+def publish_forecasts(db_path:str, targets:list, verbose:bool):
 
+    # check if output directory set up or set them up
+    data_dir = "./deploy/data/"
+    if not os.path.isdir(data_dir):
+        if verbose: print(f"Creating {data_dir}")
+        os.mkdir(data_dir)
 
-mapping_reg = {
-    'DE_50HZ' : '50hz',
-    'DE_TENNET' : 'tenn',
-    'DE_AMPRION': 'ampr',
-    'DE_TRANSNET' : 'tran',
-}
-mapping_key = {
-    'DE_50HZ' : '50Hz',
-    'DE_TENNET' : 'TenneT',
-    'DE_AMPRION' : 'Amprion',
-    'DE_TRANSNET' : 'TransnetBW',
-}
-def OLD__publish_generation(
-    target='wind_offshore',
-    regions=('DE_50HZ', 'DE_TENNET'),
-    n_folds = 3,
-    metric = 'rmse',
-    method_type = 'trained', # 'trained'
-    results_root_dir = 'forecasting_modules/output/',
-    database_dir = 'database/',
-    output_dir = 'deploy/data/forecasts/'
-):
+    data_dir_web = data_dir + "forecasts/"
+    if not os.path.isdir(data_dir_web):
+        if verbose: print(f"Creating {data_dir_web}")
+        os.mkdir(data_dir_web)
 
-    '''
-    de_regions = [
-        {'name':'DE_AMPRION','suffix':'_ampr', 'TSO':'Amprion'},
-        {'name':'DE_50HZ','suffix':'_50hz', 'TSO':'50Hertz'},
-        {'name':'DE_TENNET','suffix':'_tenn', 'TSO':'TenneT'},
-        {'name':'DE_TRANSNET','suffix':'_tran', 'TSO':'TransnetBW'},
-    ]
-    '''
+    data_dir_api = data_dir + "api/"
+    if not os.path.isdir(data_dir_api):
+        if verbose: print(f"Creating {data_dir_api}")
+        os.mkdir(data_dir_api)
 
+    for target in targets:
+        if target == 'wind_offshore': regions = ('DE_50HZ', 'DE_TENNET')
+        else: regions = ('DE_50HZ', 'DE_TENNET', 'DE_AMPRION', 'DE_TRANSNET')
 
-
-
-    # Ensemble model will be abbreviated for simplicity
-    def convert_ensemble_string(input_string):
-        # Extract the ensemble name and the components
-        ensemble_name = input_string.split('[')[1].split(']')[0]
-        components = input_string.split('(')[1].split(')')[0].split(',')
-
-        # Construct the desired format
-        output_string = f"meta_{ensemble_name}_" + "_".join(components)
-        return output_string
-
-    table = [] # to be shown in 'description'
-
-    df_results = pd.DataFrame()
-
-    for region in regions:
-        key = mapping_key[region]
-        region = mapping_reg[region]
-        var = target + '_' + region
-
-        # load sumamry metrics for different models and horizons for a given target varaible
-        df = pd.read_csv( f'{results_root_dir}{var}/summary_metrics.csv' )
-        res_models, res_drifts = analyze_model_performance(df, n_folds=n_folds, metric=metric)
-        res_models = res_models[res_models['method'] == method_type]
-        best_model:str = str(res_models['model_label'].values[0])
-        if best_model.__contains__('ensemble'):
-            best_model = convert_ensemble_string(best_model)
-
-        # load timestamp when the model was last trained
-        with open(f"{results_root_dir}{var}/{best_model}/{method_type}/datetime.json", "r") as file:
-            train_time = pd.to_datetime(json.load(file)['datetime'])
-
-        # load training properties (features etc.)
-        with open(f"{results_root_dir}{var}/{best_model}/{method_type}/metadata.json", "r") as file:
-            metadata:dict = json.load(file)
-
-        table.append({
-            'TSO/Region':key,
-            'Train Date':train_time.strftime('%Y-%m-%d'),
-            'N Features':len(metadata['features']),
-            'Best Model':best_model,
-            'Average RMSE':float(res_models[f'avg_{metric}'].values[0])
-        })
-
-        # compute total (for SMARD comparison)
-        df_res = pd.read_csv(
-            f"{results_root_dir}{var}/{best_model}/{method_type}/result.csv",
-            index_col=0, parse_dates=True)
-        df_res.columns = [ col.replace(region + '_', '') for col in df_res.columns ]
-        if df_results.empty: df_results = df_res.copy()
-        else: df_results += df_res.copy()
-
-
-    def compute_error_metrics_cutoffs(
-            df_, cutoffs:list, horizon:int, target:str, key_actual:str, key_fitted:str)->dict:
-        ''' compute errror metrics for batches separated by cutoffs'''
-        smard_metrics = {}
-        for i, cutoff in enumerate(cutoffs):
-            mask_ = (df_.index >= cutoff) & (df_.index < cutoff + pd.Timedelta(hours=horizon))
-            actual = df_[key_actual][mask_]
-            predicted = df_[key_fitted][mask_]
-            df = pd.DataFrame({
-                f'{target}_actual':actual.values,
-                f'{target}_fitted': predicted.values,
-                f'{target}_lower': np.zeros_like(actual.values),
-                f'{target}_upper': np.zeros_like(actual.values)
-            }, index=actual.index)
-            smard_metrics[cutoff] = compute_error_metrics(target, df)
-        return smard_metrics
-
-    def retain_most_recent_entries(data:dict, N:int):
-        # Sort keys by timestamp (assuming keys are sortable timestamps)
-        sorted_keys = sorted(data.keys(), reverse=True)
-        # Select the most recent N keys
-        most_recent_keys = sorted_keys[:N]
-        # Create a new dictionary with only the most recent N entries
-        recent_entries = {key: data[key] for key in most_recent_keys}
-        return recent_entries
-
-    # -------- FOR TOTAL COMPUTE ERROR OVER THE LAST N HORIZONS --------------- #
-
-
-    df = pd.read_csv( f'{results_root_dir}{var}/summary_metrics.csv' ) # get cutoffs (general for all models)
-    cutoffs = df[df['method'] == method_type]['horizon'].unique()
-    cutoffs = [pd.to_datetime(cutoff) for cutoff in cutoffs]
-    horizon = int(metadata['horizon'])
-
-    total_metrics = compute_error_metrics_cutoffs(
-        df_=df_results, cutoffs=cutoffs, horizon=horizon, target=target,
-        key_actual=f'{target}_actual', key_fitted=f'{target}_fitted'
-    )
-    total_metrics = retain_most_recent_entries(total_metrics, N=horizon)
-    ave_total_metric = np.average([total_metrics[time_s][metric] for time_s in total_metrics.keys()])
-
-    print(f'For {target} average over {n_folds} total RMSE for {target} is {ave_total_metric}')
-
-
-    # ----------- COMPUTE SMARD ERROR OVER THE LAST N HORIZONS ------------------ #
-
-    df_smard = pd.read_parquet(database_dir + 'smard/' + 'history.parquet')
-
-    smard_metrics = compute_error_metrics_cutoffs(
-        df_=df_smard,  cutoffs=cutoffs, horizon=horizon, target=target,
-        key_actual=target, key_fitted=f"{target}_forecasted"
-    )
-
-    smard_metrics = retain_most_recent_entries(smard_metrics, n_folds)
-    ave_smard_metric = np.average([smard_metrics[time_s][metric] for time_s in smard_metrics.keys()])
-
-    print(f'For {target} Average over {n_folds} SMARD RMSE for {target} is {ave_smard_metric}')
-
-    table = pd.DataFrame(table)
-
-    # ----------- CONVERT FORECASTS CSV TO JSON FOR WEBPAGE ------------------ #
-
-    possible_types = ['forecast.csv', 'result.csv']
-
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    # Convert .csv past and current forecasts into json files for each TSO
-    for region in regions:
-        key = mapping_key[region]
-        region = mapping_reg[region]
-        var = target + '_' + region
-        output_dir_ = output_dir + var + '/'
-
-        for ftype in possible_types:
-            model_label = str(table[table['TSO/Region']==key]['Best Model'].values[0])
-            df = pd.read_csv(
-                f'{results_root_dir}/{var}/{model_label}/forecast/{ftype}',
-                index_col=0,
-                parse_dates=True
-            )[:pd.Timestamp(datetime.today(),tz='UTC') + timedelta(days=7)]
-
-            convert_csv_to_json(
-                df = df,
-                target=var,
-                output_dir=output_dir_,
-                prefix = 'forecast_curr' if ftype == 'forecast.csv' else 'forecast_prev',
-                cols=[f"fitted", f"lower", f"upper"] if ftype == 'forecast.csv' else [f"actual", f"fitted", f"lower", f"upper"]#[f"actual", f"fitted", f"lower", f"upper"]
-            )
-
-    # Compute total from .csv past and current forecasts into json files for each TSO
-    output_dir_ = output_dir + target + '/'
-    for ftype in possible_types:
-        df = pd.DataFrame
-        for region in regions:
-            key = mapping_key[region]
-            region = mapping_reg[region]
-            var = target + '_' + region
-
-            model_label = str(table[table['TSO/Region']==key]['Best Model'].values[0])
-            df1 = pd.read_csv(
-                f'{results_root_dir}/{var}/{model_label}/forecast/{ftype}',
-                index_col=0,
-                parse_dates=True
-            )[:pd.Timestamp(datetime.today(),tz='UTC') + timedelta(days=7)]
-            df1.columns = [ col.replace(f'_{region}', '') for col in df1.columns ]
-
-            if df.empty:df = df1.copy()
-            else: df = df + df1.copy()
-
-        convert_csv_to_json(
-            df = df,
+        # publish data to webpage
+        publish_generation(
             target=target,
-            output_dir=output_dir_,
-            prefix = 'forecast_curr' if ftype == 'forecast.csv' else 'forecast_prev',
-            cols=[f"fitted", f"lower", f"upper"] if ftype == 'forecast.csv' else [f"actual", f"fitted", f"lower", f"upper"]#[f"actual", f"fitted", f"lower", f"upper"]
-        )
-
-
-    # print(table)
-    # Rename values starting with 'meta_' to 'Ensemble'
-    table["Best Model"] = table["Best Model"].apply(lambda x: "Ensemble" if x.startswith("meta_") else x)
-    # Round floating point values to integers
-    table["Average RMSE"] = table["Average RMSE"].round().astype(int)
-    # Save as markdown
-    summary_fpath = f'{output_dir}/{target}_notes_en.md'
-    table.to_markdown(summary_fpath, index=False)
-
-
-    intro_sentences = \
-    f"""
-Our __week-ahead__ forecast has average RMSE of __{ave_total_metric:.0f}__.  
-SMARD __day-ahead__ forecast has average accuracy of __{ave_smard_metric:.0f}__. 
-    """
-
-    # Reading the markdown content
-    with open(summary_fpath, "r") as file:
-        markdown_content = file.read()
-    # Prepending the sentences to the markdown content
-    updated_markdown_content = intro_sentences + "\n" + markdown_content
-    # Saving the updated markdown content
-    with open(summary_fpath, "w") as file:
-        file.write(updated_markdown_content)
-
-
-    # -------------- GERMAN TEXT -------------------
-
-    dictionary_en_de = {
-        "Best Model": "Bestes Modell",
-        "Average RMSE": "Durchschnittlicher RMSE",
-        "TSO/Region": "ÜNB/Region",
-        "Train Date": "Trainingsdatum",
-        "N Features": "Anzahl der Merkmale"
-    }
-    table.rename(columns = dictionary_en_de, inplace = True)
-
-    summary_fpath = f'{output_dir}/{target}_notes_de.md'
-    table.to_markdown(summary_fpath, index=False)
-
-    intro_sentences = f"""
-Unsere __Wochenprognose__ hat einen durchschnittlichen RMSE von __{ave_total_metric:.0f}__.  
-Die SMARD __Tagesprognose__ weist eine durchschnittliche Genauigkeit von __{ave_smard_metric:.0f}__ auf.
-    """
-
-    # Reading the markdown content
-    with open(summary_fpath, "r") as file:
-        markdown_content = file.read()
-    # Prepending the sentences to the markdown content
-    updated_markdown_content = intro_sentences + "\n" + markdown_content
-    # Saving the updated markdown content
-    with open(summary_fpath, "w") as file:
-        file.write(updated_markdown_content)
-
-class PublishDataForDeployment:
-    pass
-
-if __name__ == '__main__':
-    publish_generation(
-        target='wind_offshore',
-        regions=('DE_50HZ', 'DE_TENNET'),
-        n_folds = 3,
-        metric = 'rmse',
-        method_type = 'trained', # 'trained'
-        results_root_dir = 'forecasting_modules/output/',
-        database_dir = 'database/',
-        output_dir = 'deploy/data/forecasts/'
-    )
-
-    publish_generation(
-            target='wind_offshore',
-            regions=('DE_50HZ', 'DE_TENNET', 'DE_AMPRION', 'DE_TRANSNET'),
+            avail_regions=regions,
             n_folds = 3,
             metric = 'rmse',
             method_type = 'trained', # 'trained'
-            results_root_dir = 'forecasting_modules/output/',
-            database_dir = 'database/',
-            output_dir = 'deploy/data/forecasts/'
-    )
+            results_root_dir = './output/forecasts/',
+            database_dir = db_path,
+            output_dir = data_dir_web
+        )
+
+        # publish to API folder
+        publish_to_api(
+            target=target,
+            avail_regions=regions,
+            method_type = 'forecast', # 'trained'
+            results_root_dir = './output/forecasts/',
+            output_dir = f'{data_dir_api}forecasts/'
+        )
+
+if __name__ == '__main__':
+    db_path = './database/'
+
+    targets = ['wind_offshore', 'wind_onshore', 'solar', 'load']
+
+    publish_forecasts(targets=targets, db_path=db_path, verbose=True)
+
+    print(f"All tasks in update are completed successfully!")
--- update_forecasts.py
+++ update_forecasts.py
@@ -4,84 +4,14 @@ from forecasting_modules import (
     update_forecast_production
 )
 
-from publish_data import (
-    publish_generation,
-    publish_to_api
-)
 
-
-def update_forecasts(db_path:str, targets:list, verbose:bool):
-
-    for target in targets:
-        update_forecast_production(
-            database=db_path, variable=target, outdir='./output/forecasts/', verbose=verbose
-        )
-
-def publish_forecasts(db_path:str, targets:list, verbose:bool):
-
-    # check if output directory set up or set them up
-    data_dir = "./deploy/data/"
-    if not os.path.isdir(data_dir):
-        if verbose: print(f"Creating {data_dir}")
-        os.mkdir(data_dir)
-
-    data_dir_web = data_dir + "forecasts/"
-    if not os.path.isdir(data_dir_web):
-        if verbose: print(f"Creating {data_dir_web}")
-        os.mkdir(data_dir_web)
-
-    data_dir_api = data_dir + "api/"
-    if not os.path.isdir(data_dir_api):
-        if verbose: print(f"Creating {data_dir_api}")
-        os.mkdir(data_dir_api)
-
-    for target in targets:
-        if target == 'wind_offshore': regions = ('DE_50HZ', 'DE_TENNET')
-        else: regions = ('DE_50HZ', 'DE_TENNET', 'DE_AMPRION', 'DE_TRANSNET')
-
-        # publish data to webpage
-        publish_generation(
-            target=target,
-            avail_regions=regions,
-            n_folds = 3,
-            metric = 'rmse',
-            method_type = 'trained', # 'trained'
-            results_root_dir = './output/forecasts/',
-            database_dir = db_path,
-            output_dir = data_dir_web
-        )
-
-        # publish to API folder
-        publish_to_api(
-            target=target,
-            avail_regions=regions,
-            method_type = 'forecast', # 'trained'
-            results_root_dir = './output/forecasts/',
-            output_dir = f'{data_dir_api}forecasts/'
-        )
-
-def main():
-
+if __name__ == '__main__':
     db_path = './database/'
 
     targets = ['wind_offshore', 'wind_onshore', 'solar', 'load']
-
-    update_forecasts(targets=targets, db_path=db_path, verbose=True)
-
-    publish_forecasts(targets=targets, db_path=db_path, verbose=True)
-
-    print(f"All tasks in update are completed successfully!")
-
-def tst_main():
-
-    db_path = './database/'
-
-    update_forecasts(targets=['energy_mix'], db_path=db_path, verbose=True)
-
-    # publish_forecasts(targets=targets, db_path=db_path, verbose=True)
+    for target in targets:
+        update_forecast_production(
+            database=db_path, variable=target, outdir='./output/forecasts/', verbose=True
+        )
 
     print(f"All tasks in update are completed successfully!")
-
-if __name__ == '__main__':
-    print("launching update_forecasts.py")
-    main()
